d9124adebdbbfdda412e7383687d45fd0844e416
espinozaheleno <eespinozahellen@gmail.com>
2025-08-13 20:13:43 -0600

    add: Styles

--- Proyecto final/src/exploracion_inicial.py
+++ Proyecto final/src/exploracion_inicial.py
@@ -3,29 +3,29 @@ import pandas as pd
 def cargar_dataset(ruta_archivo):
     try:
         df = pd.read_csv(ruta_archivo)
-        print("✅ Dataset cargado correctamente.\n")
+        print(" Dataset cargado correctamente.\n")
         return df
     except FileNotFoundError:
-        print("❌ Error: Archivo no encontrado. Verifica el nombre o la ruta.")
+        print(" Error: Archivo no encontrado. Verifica el nombre o la ruta.")
     except pd.errors.ParserError:
-        print("❌ Error: Formato incorrecto o archivo corrupto.")
+        print(" Error: Formato incorrecto o archivo corrupto.")
     except Exception as e:
-        print(f"❌ Error inesperado: {e}")
+        print(f" Error inesperado: {e}")
 
 def exploracion_basica(df):
-    print("🔍 Dimensiones del dataset (filas, columnas):")
+    print(" Dimensiones del dataset (filas, columnas):")
     print(df.shape)
 
-    print("\n🧩 Tipos de datos por columna:")
+    print("\n Tipos de datos por columna:")
     print(df.dtypes)
 
-    print("\n📊 Estadísticas descriptivas:")
+    print("\n Estadísticas descriptivas:")
     print(df.describe(include='all'))
 
-    print("\n🕵️‍♂️ Conteo de valores nulos por columna:")
+    print("\n Conteo de valores nulos por columna:")
     print(df.isnull().sum())
 
-    print("\n📝 Primeras 5 filas del dataset:")
+    print("\n Primeras 5 filas del dataset:")
     print(df.head())
 
 if __name__ == "__main__":
@@ -33,5 +33,6 @@ if __name__ == "__main__":
     datos = cargar_dataset(ruta_csv)
     if datos is not None:
         exploracion_basica(datos)
+        
 
 
